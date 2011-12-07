5c19ed67ed2629d4b70c3a62c2bb644c04f42749
Johannes Larsen <johs.a.larsen@gmail.com>
2011-12-07 14:13:53 +0100

    dmath.h,list.h: removed comment spacing, resulting in improved readability

--- dmath.h
+++ dmath.h
@@ -3,37 +3,28 @@
 
 /* Discrete mathematics */
 
-/*
- * Returns the nth fibonacci number (0, 1, 1, 3, ...)
+/* Returns the nth fibonacci number (0, 1, 1, 3, ...)
  * -EDOM if n < 0, -ERANGE if n > 92 (causes overflow)
- * for more info: http://en.wikipedia.org/wiki/Fibonacci_number
- */
+ * for more info: http://en.wikipedia.org/wiki/Fibonacci_number */
 long long fibonacci(int n);
 
-/*
- * Greatest common divisor is the greatest integer which divides both a and b without a remainder
- */
+/* Greatest common divisor is the greatest integer which divides both a and b without a remainder */
 int gcd(int a, int b);
 
-/*
- * Least common multiple is the smallest positive integer which is divisable by both a and b without a remainder
- */
+/* Least common multiple is the smallest positive integer which is divisable by both a and b without a remainder */
 long long lcm(int a, int b);
 
-/*
- * Integers are relative prime if gcd(a,b) = 1, so no common prime factors
- */
+/* Integers are relative prime if gcd(a,b) = 1, so no common prime factors */
 int arerelativeprime(int a, int b);
 
-/*
- * Chinese remainder is a mathematical theorem to calculate x in an equation system like this:
+
+/* Chinese remainder is a mathematical theorem to calculate x in an equation system like this:
  *   x \equiv a_1 % m_1
  *   x \equiv a_2 % m_2
  *      ...
  *   x \equiv a_neq % m_neq
  *
- * m_1..m_neq must be pairwise relative prime (gcd(m_1, m-2) = 1, ...)
- */
+ * m_1..m_neq must be pairwise relative prime (gcd(m_1, m-2) = 1, ...) */
 int chinese_remainder(const int *a, const int *m, int neq);
 
 int isprime(unsigned int n);
--- list.h
+++ list.h
@@ -1,74 +1,48 @@
 #ifndef LIST_H_
 #define LIST_H_
 
-/*
- * List interface
- */
+/* List interface */
 
 typedef struct List List;
 
-/*
- * Create new list
- */
+/* Create new list */
 List *list_create(void);
 
-/*
- * Free list. 
- * All nodes are freed, but items pointed to by nodes are preserved.
- */
+/* Free list. 
+ * All nodes are freed, but items pointed to by nodes are preserved. */
 int list_destroy(List *list);
 
-/*
- * Insert item first in list
- */
+/* Insert item first in list */
 int list_prepend(List *list, void *item);
 
-/*
- * Insert item last in list
- */
+/* Insert item last in list */
 int list_append(List *list, void *item);
 
-/*
- * Remove object from list
- */
+/* Remove object from list */
 int list_remove(List *list, void *item);
 
-/*
- * Pop the first item off the list
- * The node that points to the item popped will be removed
- */
+/* Pop the first item off the list
+ * The node that points to the item popped will be removed */
 void *list_pop(List *list);
 
-/*
- * Return # of items in list
- */
+/* Return # of items in list */
 int list_size(List *list);
 
 
-/*
- * List iterator interface
- */
+/* List iterator interface */
  
 typedef struct List_iterator List_iterator;
 
-/*
- * Create new list iterator
- */
+/* Create new list iterator */
 List_iterator *list_createiterator(List *list);
 
-/*
- * Free iterator
- */
+/* Free iterator */
 int list_destroyiterator(List_iterator *li);
 
-/*
- * Move iterator to next item and return current
- */
+/* Move iterator to next item and return current */
 void *list_next(List_iterator *li);
 
-/*
- * Let iterator point to first item in list
- */
+/* Let iterator point to first item in list */
 int list_resetiterator(List_iterator *li);
 
 #endif /*LIST_H_*/
