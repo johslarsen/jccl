586b7e8b824890f6a423e7af486e13ad9baebb0b
Johannes Larsen <mail@johslarsen.net>
2015-06-07 19:53:30 +0200

    created a O(n*k) bucket sorting algorithm

--- sort.c
+++ sort.c
@@ -1,14 +1,20 @@
 #include "CuTest/CuTest.h"
 #include "sort.h"
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ODD(n) (n & 1)
 
-void swap(int *a, int *b) {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+inline static void swap(void *a, void *b, size_t size) {
+	char *a_byte = (char *)a;
+	char *b_byte = (char *)b;
+	do {
+		char tmp = *a_byte;
+		*a_byte++ = *b_byte;
+		*b_byte++ = tmp;
+	} while (--size > 0);
 }
 
 void sort_odd_even(int *a, int na) {
@@ -24,25 +30,30 @@ void sort_odd_even(int *a, int na) {
 
 	for ( ; i < na; i++) {
 		if ( ODD(a[i]) ) { // odd
-			swap(first_even++, &a[i]);
+			swap(first_even++, &a[i], sizeof(*a));
 		}
 	}
 }
 
-static int issorted_odd_even(int *a, int na) {
-	int odd = 1, *p = a;
-	int *eoa = p+na;
-	for (; p < eoa; p++) {
-		if ( ODD(*p) ) {
-			if ( !odd ) {
-				return 0;
-			}
+static int issorted(void *base, size_t nmemb, size_t size, bsort_categorizer categorizer) {
+	size_t max = 0;
+
+	void *eobase = base + (nmemb*size);
+	printf("\n");
+	for (void *p = base; p < eobase; p += size) {
+		size_t bucket = categorizer(p);
+		printf("%8x %8x\n", *((int *)p), bucket);
+		if (bucket < max) {
+			return 0;
 		} else {
-			odd = 0;
+			max = bucket;
 		}
 	}
-
 	return 1;
+}
+
+static size_t bsort_categorize_odd_even(const void *a) {
+	return !ODD((*((const int *)a)));
 }
 
 void TestSort_odd_even(CuTest *tc) {
@@ -55,7 +66,72 @@ void TestSort_odd_even(CuTest *tc) {
 		ints[i] = rand();
 	}
 
-	CuAssertIntEquals(tc, 0, issorted_odd_even(ints, NINT));
+	CuAssertIntEquals(tc, 0, issorted(ints, NINT, sizeof(*ints), bsort_categorize_odd_even));
 	sort_odd_even(ints, NINT);
-	CuAssertIntEquals(tc, 1, issorted_odd_even(ints, NINT));
+	CuAssertIntEquals(tc, 1, issorted(ints, NINT, sizeof(*ints), bsort_categorize_odd_even));
+}
+
+
+inline static void *bsort_end_of_sorted_base(void *base, size_t nmemb, size_t size, bsort_categorizer categorizer, size_t nbucket, void *buckets[]) {
+	size_t last_bucket = 0;
+	buckets[last_bucket] = base;
+
+	void *p, *eobase = base + (nmemb*size);
+	for (p = base; p < eobase; p += size) {
+		size_t bucket = categorizer(p);
+		assert(bucket < nbucket);
+
+		if (bucket < last_bucket) {
+			break; // i.e. from here on out base is unsorted
+		} else if (bucket > last_bucket) {
+			for (int i = last_bucket+1; i <= bucket; i++) {
+				buckets[i] = p;
+			}
+			last_bucket = bucket;
+		}
+	}
+	for (int i = last_bucket+1; i < nbucket; i++) {
+		buckets[i] = p;
+	}
+
+	return p;
+}
+
+void bsort(void *base, size_t nmemb, size_t size, bsort_categorizer categorizer, size_t nbucket, void *buckets[]) {
+	void *eobase = base + (nmemb*size);
+	void *base_unsorted = bsort_end_of_sorted_base(base, nmemb, size, categorizer, nbucket, buckets);
+	for (void *p = base_unsorted; p < eobase; p += size) {
+		size_t bucket = categorizer(p);
+		assert(bucket < nbucket);
+
+		void *slot = p;
+		for (int i = nbucket-1; i > bucket; i--) {
+			if (slot != buckets[i]) {
+				swap(buckets[i], slot, size);
+				slot = buckets[i];
+			}
+			buckets[i] += size;
+		}
+	}
+}
+
+size_t bsort_categorize_least_significant_nibble(const void *a) {
+	return (*((const int *)a))&0xf;
+}
+
+void TestBsort_odd_even(CuTest *tc) {
+	enum {
+		NINT = 100,
+	};
+
+	int ints[NINT];
+	for (int i = 0; i < NINT; i++) {
+		ints[i] = rand();
+	}
+
+	int *buckets[0x10];
+
+	CuAssertIntEquals(tc, 0, issorted(ints, NINT, sizeof(*ints), bsort_categorize_least_significant_nibble));
+	bsort(ints, NINT, sizeof(*ints), bsort_categorize_least_significant_nibble, 0x10, (void **)buckets);
+	CuAssertIntEquals(tc, 1, issorted(ints, NINT, sizeof(*ints), bsort_categorize_least_significant_nibble));
 }
--- sort.h
+++ sort.h
@@ -1,5 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
+#include <stddef.h>
 
 /*
  * sort an array of integers so that odd numbers come before even numbers, the
@@ -9,5 +10,28 @@
  * en funksjon som organiserer tallene i A slik at oddetall kommer før partall."
  */
 void sort_odd_even(int *a, int na);
+
+/*
+ * categorize some element a
+ *
+ * returns:
+ *   n <-- 0 <= n < nbucket (supplied to bsort)
+ */
+typedef size_t (*bsort_categorizer)(const void *a);
+
+/*
+ * a generalized in-place bucket sorting algorithm.
+ *
+ * this is an exchange algorithm that is unstable (i.e. does not maintain
+ * relative order). it has a worst performance O(nmemb*(nbucket-1)) and a best
+ * performance O(nmemb) when the input is presorted. it is non-recursive and
+ * being in-place it only requires O(1) additional space.
+ *
+ * it requires a non-NULL buckets that fits nbucket pointers. after completion
+ * this contains pointers to the first element of the given category. being an
+ * in-place algorithm categories will overlap, and empty categories points to
+ * the same element as the next category or at the end of the base array.
+ */
+void bsort(void *base, size_t nmemb, size_t size, bsort_categorizer categorizer, size_t nbucket, void *buckets[]);
 
 #endif /* SORT_H */
